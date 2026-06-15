56797a4bba3a65030f72d65c1c42a2846ae04149
damusiri2 <damu.sri5@gmail.com>
2026-06-15 20:25:24 +0530

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -10,3 +10,36 @@ resource "aws_vpc" "com_vpc" {
     Name = "com-vpc"
   }
 }
+
+# =========================
+# SUBNETS
+# =========================
+
+resource "aws_subnet" "com_web_sn" {
+  vpc_id                  = aws_vpc.com_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "com-web-subnet"
+  }
+}
+
+resource "aws_subnet" "com_api_sn" {
+  vpc_id                  = aws_vpc.com_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "com-api-subnet"
+  }
+}
+
+resource "aws_subnet" "com_db_sn" {
+  vpc_id     = aws_vpc.com_vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "com-db-subnet"
+  }
+}
