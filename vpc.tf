ed3381a9ad00be0a630fc6edf22adb7cb625440f
damusiri2 <damu.sri5@gmail.com>
2026-06-15 20:42:03 +0530

    create nacls

--- vpc.tf
+++ vpc.tf
@@ -99,3 +99,112 @@ resource "aws_route_table_association" "com_db_assoc" {
   subnet_id      = aws_subnet.com_db_sn.id
   route_table_id = aws_route_table.com_private_rt.id
 }
+
+# =====================
+# COM WEB NACL
+# =====================
+
+resource "aws_network_acl" "web_nacl" {
+  vpc_id = aws_vpc.com_vpc.id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "com-web-nacl"
+  }
+}
+
+# =====================
+# COM API NACL
+# =====================
+
+resource "aws_network_acl" "api_nacl" {
+  vpc_id = aws_vpc.com_vpc.id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "com-api-nacl"
+  }
+}
+
+# =====================
+# COM DB NACL
+# =====================
+
+resource "aws_network_acl" "db_nacl" {
+  vpc_id = aws_vpc.com_vpc.id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "com-db-nacl"
+  }
+}
+
+# =====================
+# COM NACL ASSOCIATIONS
+# =====================
+
+resource "aws_network_acl_association" "web_nacl_assoc" {
+  network_acl_id = aws_network_acl.web_nacl.id
+  subnet_id      = aws_subnet.web_subnet.id
+}
+
+resource "aws_network_acl_association" "api_nacl_assoc" {
+  network_acl_id = aws_network_acl.api_nacl.id
+  subnet_id      = aws_subnet.api_subnet.id
+}
+
+resource "aws_network_acl_association" "db_nacl_assoc" {
+  network_acl_id = aws_network_acl.db_nacl.id
+  subnet_id      = aws_subnet.db_subnet.id
+}
