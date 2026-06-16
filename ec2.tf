4941d966280b7dee6a4345f2d2aa52c74fca9738
damusiri2 <damu.sri5@gmail.com>
2026-06-16 23:02:21 +0530

    web server setup

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+# EC2 Web Server
+
+resource "aws_instance" "com_web_server" {
+
+  ami           = "ami-xxxxxxxxxxxxxxxxx"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.com_web_sn.id
+
+  key_name = "eks-key.pem"
+
+  vpc_security_group_ids = [
+    aws_security_group.com_web_sg.id
+  ]
+
+  user_data = file("setup.sh")
+
+  tags = {
+    Name = "com-web-server"
+  }
+}
